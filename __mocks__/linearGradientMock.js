2f54427fedd84cc0d92bc8f9b1fa346cd42ebb4b
Prashant Shaw <prashant.shaw926@gmail.com>
2026-04-06 21:46:39 +0530

    chore: remove Jest configuration and setup files

--- __mocks__/linearGradientMock.js
+++
@@ -1,8 +0,0 @@
-const React = require('react');
-
-function LinearGradientMock({ children }) {
-  return React.createElement('LinearGradient', null, children);
-}
-
-module.exports = LinearGradientMock;
-module.exports.default = LinearGradientMock;
--- __mocks__/svgMock.js
+++
@@ -1,8 +0,0 @@
-const React = require('react');
-
-function SvgMock(props) {
-  return React.createElement('SvgMock', props, props.children);
-}
-
-module.exports = SvgMock;
-module.exports.default = SvgMock;
